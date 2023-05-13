de8e0045d1fc1875e4bc78ead8c25827baf2f052
kathechan <katherineperez125@gmail.com>
2023-05-13 14:56:47 -0400

    Cambio 307

--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,39 @@
-const express = require('express');
-const router = express.Router();
-const sqlite3=require('sqlite3').verbose();
-const  path = require('path');
 
 
-const basededatos=path.join(__dirname,"basededatos","basededatos.db");
-const bd=new sqlite3.Database(basededatos, err =>{ 
-if (err){
-	return console.error(err.message);
-}else{
-	console.log("db only");
-}
-})
+const sqlite3 = require("sqlite3").verbose();
 
-const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT, country VARCHAR(20);";
-
-bd.run(create,err=>{
-	if (err){
-	return console.error(err.message);
-}else{
-	console.log("table only");
-}
-})
-
-router.get('/contactos',(req,res)=>{
-	const sql="SELECT * FROM contactos;";
-	bd.all(sql, [],(err, rows)=>{
-			if (err){
-				return console.error(err.message);
-			}else{
-			res.render("contactos.ejs",{datos:rows});
-			}
-	})
-})
-	//Obtener la fecha/hora
-	var hoy = new Date();
-	var horas = hoy.getHours();
-	var minutos = hoy.getMinutes();
-	var segundos = hoy.getSeconds();
-  	var hora = horas + ':' + minutos + ':' + segundos + ' ';
-  	var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '//' + hora;
-	  //////////////Obtener la IP publica////////////////
-	  var ip = req.headers['x-forwarded-for'] || rep.sockect.remoteAddress;
-	  if (ip){
-		var list = ip.split(",");
-		ip = list[list.length-1];
-	 } else {
-		ip = req.connection.remoteAddress;
-	  }
-
-		//Ingreso de los registros hacia la Base de Datos
-	const sql="INSERT INTO contactos(nombre, email, comentario, fecha,ip) VALUES (?,?,?,?,?)";
-	const nuevos_mensajes=[req.body.nombre, req.body.email, req.body.comentario,fecha,ip];
-	db.run(sql, nuevos_mensajes, err =>{
-	if (err){
+let db = new sqlite3.Database(__dirname + "/database.db", (err) => {
+	if (err) {
 		return console.error(err.message);
 	}
-	else{
-		res.redirect('/');
-		}
-	})
+console.log("Connected to the Sqlite database.");
 
-
-
-
-
-
-router.get('/',(req,res)=>{
-	res.render('index.ejs',{datos:{}})
+db.run(
+	"CREATE TABLE IF NOT EXIST contactos (id INTERGER PRIMARY KEY AUTOINCREMET, name TEXT NOT NULL, email TEXT NOT NULL, comment TEXT NOT NULL, date TEXT NOT NULL, ip TEXT NOT NULL)"
+);
 });
 
-
-
-module.exports = router;
+module.exports - {
+	insert: function (name, email, comment, date, ip) {
+	db.run(
+		"INSERT INTO contactos (name, email, comment, date, ip)	VALUES (?, ?, ?, ?, ?)",
+		[name, email, comment, date, ip],
+		function (err) { 
+			if (err) {
+				return console.log(err.message);
+			}
+			// get the last insert id
+			console.log ('A row has been inserted with rowid ${this.lastID}');
+	    }
+	
+	);
+  },
+  select: function (callback) {
+	db.all("SELECT * DROM contactos", [], (err, rows) =>{
+		if (err) {
+			throw err;
+		}
+		callback(rows);
+	});
+  },
+};  
