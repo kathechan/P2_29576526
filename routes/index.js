05e392bdbd9a0416240eb7a3673d272f828cf351
kathechan <katherineperez125@gmail.com>
2023-05-12 23:14:07 -0400

    Cambio 305

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sqlite3=require('sqlite3').verbose();
-
+const  path = require('path');
 
 
 const basededatos=path.join(__dirname,"basededatos","basededatos.db");
