d7a5c21c0c0b845d43216adb2dc5b6d20e895066
kathechan <katherineperez125@gmail.com>
2023-05-13 21:05:34 -0400

    Cambio 331

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const logicaDB = require('./logicaDB');
-const { Database } = require('sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
