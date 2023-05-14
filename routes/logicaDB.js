42ebcef093b4793f167398ecf773f711a6ae940a
kathechan <katherineperez125@gmail.com>
2023-05-13 22:56:34 -0400

    Cambio 337

--- routes/logicaDB.js
+++ routes/logicaDB.js
@@ -1,41 +1,31 @@
+const sqlite3 = require('sqlite3').verbose();
 
-const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
 
-
-let db = new sqlite3.Database(__dirname + "/database.db", (err) => {
-	if (err) {
-		return console.error(err.message);
-	}
-console.log("Connected to the Sqlite database.");
-
-db.run(
-	"CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT);"
-);
+    db.run("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, comment TEXT NOT NULL, date TEXT NOT NULL, ip TEXT NOT NULL)");
 });
 
-module.exports - {
-	insert: function (name, email, comment, date, ip) {
-	db.run(
-		"INSERT INTO contactos (name, email, comment, date, ip)	VALUES (?, ?, ?, ?, ?)",
-		[name, email, comment, date, ip],
-		function (err) { 
-			if (err) {
-				return console.log(err.message);
-			}
-			// get the last insert id
-			console.log ('A row has been inserted with rowid ${this.lastID}');
-	    }
-	
-	);
-  },
-  select: function (callback) {
-	db.all("SELECT * FROM contactos", [], (err, rows) =>{
-		if (err) {
-			throw err;
-		}
-		callback(rows);
-	});
-  },
-};  
 
-
+module.exports = {
+    insert: function (name, email, comment, date, ip) {
+        db.run("INSERT INTO contactos (name, email, comment, date, ip) VALUES (?, ?, ?, ?, ?)", [name, email, comment, date, ip], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    },
+    select: function (callback) {
+        db.all("SELECT * FROM contactos", [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            callback(rows);
+        });
+    }
+}
