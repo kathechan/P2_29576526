d7c49bfddde2d66f4834b114731213813965b6b8
kathechan <katherineperez125@gmail.com>
2023-05-29 01:24:06 -0400

    Cambio 354

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,81 @@
 const express = require('express');
 const router = express.Router();
 const logicaDB = require('./logicaDB');
-const db = ('./logicaDB')
+const db = ('./logicaDB');
+const axios = require('axios');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let name = 'Katherine Perez'
   res.render('index', { title: 'Express' });
 });
+const API_KEY = 'd1a67fd9aedb3aded415ca7c1909f1e3';
+
+function getCountryFromIP(ip) {
+  const url = `http://api.ipstack.com/${ip}?access_key=${API_KEY}`;
+
+  return axios.get(url)
+    .then(response => {
+      const country = response.data.country_name;
+      return country;
+    })
+    .catch(error => {
+      console.log(error);
+      throw new Error('Error al obtener la ubicación del usuario.');
+    });
+}
+
+router.get('/api/ipstack/:ip', (req, res) => {
+  const ip = req.params.ip;
+  const url = `http://api.ipstack.com/${ip}?access_key=${API_KEY}`;
+
+  axios.get(url)
+    .then(response => {
+      const country = response.data.country_name;
+      res.json({ country });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error al obtener la ubicación del usuario.' });
+    });
+});
 
 router.post('/', function (req, res, next) {
   let name = req.body.name;
   let email = req.body.email;
   let comment = req.body.comment;
-  let date = new Date(); // @todo falta formatear la fecha
-  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // @todo falta formatear la ip
+  let date = new Date(); 
+  let formattedDate = date.toLocaleDateString("es-ES");
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  function formatIP(ip) {
+    const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+    if (!ipRegex.test(ip)) {
+      throw new Error('Dirección IP inválida');
+    }
 
-  logicaDB.insert(name, email, comment, date, ip);
+    const parts = ip.split('.');
+    const formattedParts = parts.map(part => {
+      return part.padStart(3, '0');
+    });
+    const formattedIP = formattedParts.join('.');
+    return formattedIP;
+  }
 
-  console.log({ name, email, comment, date, ip});
-
-  res.redirect('/');
+  getCountryFromIP(ip)
+  .then(country => {
+    logicaDB.insert(name, email, comment, date, ip, country);
+    console.log({ name, email, comment, date, ip, country });
+    res.redirect('/');
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).json({ message: 'Error al obtener la ubicación del usuario.' });
+  });
 });
+
 
 router.get('/contactos', function(req, res, next) {
   db.select(function (rows) {
--- routes/logicaDB.js
+++ routes/logicaDB.js
@@ -6,13 +6,13 @@ let db = new sqlite3.Database(':memory:', (err) => {
     }
     console.log('Connected to the in-memory SQlite database.');
 
-    db.run("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT NOT NULL, comment TEXT, date TEXT NOT NULL, ip TEXT NOT NULL)");
+    db.run("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT NOT NULL, comment TEXT, date TEXT NOT NULL, ip TEXT NOT NULL, country TEXT NOT NULL)");
 });
 
 
 module.exports = {
     insert: function (name, email, comment, date, ip) {
-        db.run("INSERT INTO contactos (name, email, comment, date, ip) VALUES (?, ?, ?, ?, ?)", [name, email, comment, date, ip], function (err) {
+        db.run("INSERT INTO contactos (name, email, comment, date, ip, country) VALUES (?, ?, ?, ?, ?, ?)", [name, email, comment, date, ip, country], function (err) {
             if (err) {
                 return console.log(err.message);
             }
