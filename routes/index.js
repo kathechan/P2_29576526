cd1e1f02cc3dc06468236bc46d844f93669deab2
kathechan <katherineperez125@gmail.com>
2023-05-29 18:44:15 -0400

    Cambio 367

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,15 @@ router.post('/', function (req, res, next) {
   let formattedDate = date.toLocaleDateString("es-ES");
   let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
    ip = formatIP(ip);
-   function formatIP(ip) {
-    const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    if (ipRegex.test(ip)) {
-      return ip.match(ipRegex)[0];
-    } else {
-      return ip;
-    }
-  }
+   if(ip){
+    let ip_ls = ip.split(",");
+    ip = ip_ls[ip_ls.length -1];
+}
+else{
+    console.log("IP no se pudo formatear");
+}
+});
+  
   
   function getCountryFromIP(ip) {
     const url = `http://api.ipstack.com/${ip}?access_key=${API_KEY}`;
@@ -65,7 +66,6 @@ router.post('/', function (req, res, next) {
     console.error(error);
     res.status(500).json({ message: 'Error al obtener la ubicación del usuario.' });
   });
-});
 
 
 router.get('/contactos', function(req, res, next) {
