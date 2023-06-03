8ca78b7a40a88d8d6ecd079a5097ce737bc6d7be
kathechan <katherineperez125@gmail.com>
2023-06-03 01:58:25 -0400

    Cambio 395

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const logicaDB = require('./logicaDB');
 const axios = require('axios');
 const fetch = require('node-fetch');
 
-
 router.get('/', function(req, res, next) {
-  let name = 'Katherine Perez'
+  let name = 'Katherine Perez';
   res.render('index', { title: 'Express' });
 });
 
@@ -20,10 +19,47 @@ router.post('/', (req, res) => {
     .then(response => response.json())
     .then(google_response => {
       if (google_response.success == true) {
+        let name = req.body.name;
+        let email = req.body.email;
+        let comment = req.body.comment;
+        let date = new Date(); 
+        let formattedDate = date.toLocaleDateString("es-ES");
+        let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        if(ip){
+            let ip_ls = ip.split(",");
+            ip = ip_ls[ip_ls.length -1];
+        }
+        else{
+            console.log("IP no se pudo formatear");
+        }
+        function getCountryFromIP(ip) {
+            const API_KEY = 'bbf9610212ee092c996e920fe458f171';
+            const url = `http://api.ipstack.com/${ip}?access_key=${API_KEY}`;
+          
+            return axios.get(url)
+              .then(response => {
+                const data = response.data;
+                const country = response.data.country_name;
+                return country;
+              })
+              .catch(error => {
+                console.log(error);
+                throw new Error('Error al obtener la ubicación del usuario.');
+              
+            });
+        }
         
-        res.send("El desafío de reCAPTCHA se ha completado correctamente");
+        getCountryFromIP(ip)
+        .then(country => {
+            logicaDB.insert(name, email, comment, date, ip, country);
+            console.log({ name, email, comment, date, ip, country });
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ message: 'Error al obtener la ubicación del usuario.' });
+        });
       } else {
-        
         res.send("El desafío de reCAPTCHA no se ha completado correctamente");
       }
     })
@@ -33,49 +69,6 @@ router.post('/', (req, res) => {
     });
 });
  
-router.post('/', function (req, res, next) {
-  let name = req.body.name;
-  let email = req.body.email;
-  let comment = req.body.comment;
-  let date = new Date(); 
-  let formattedDate = date.toLocaleDateString("es-ES");
-  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  if(ip){
-    let ip_ls = ip.split(",");
-    ip = ip_ls[ip_ls.length -1];
-}
-else{
-    console.log("IP no se pudo formatear");
-}
-  function getCountryFromIP(ip) {
-    const API_KEY = 'bbf9610212ee092c996e920fe458f171';
-    const url = `http://api.ipstack.com/${ip}?access_key=${API_KEY}`;
-  
-    return axios.get(url)
-      .then(response => {
-        const data = response.data;
-        const country = response.data.country_name;
-        return country;
-      })
-      .catch(error => {
-        console.log(error);
-        throw new Error('Error al obtener la ubicación del usuario.');
-      
-    });
-  }
-  
-  getCountryFromIP(ip)
-  .then(country => {
-    logicaDB.insert(name, email, comment, date, ip, country);
-    console.log({ name, email, comment, date, ip, country });
-    res.redirect('/');
-  })
-  .catch(error => {
-    console.error(error);
-    res.status(500).json({ message: 'Error al obtener la ubicación del usuario.' });
-  });
-});
-
 router.get('/contactos', function(req, res, next) {
   logicaDB.select(function (rows) {
     console.log(rows);
