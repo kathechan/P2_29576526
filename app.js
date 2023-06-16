b1e48fb6d459063e825abd84eb9cfa3758ec2791
kathechan <katherineperez125@gmail.com>
2023-06-15 23:35:33 -0400

    Cambio 400

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.get('/contactos', function(req, res) {
+  res.render('contactos');
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
